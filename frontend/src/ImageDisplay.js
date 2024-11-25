a033694f97ea867052f035855830bbcf5627b3d8
Ashinee-20 <100423063+Ashinee-20@users.noreply.github.com>
2024-11-26 02:52:30 +0530

    Update ImageDisplay.js

--- frontend/src/ImageDisplay.js
+++ frontend/src/ImageDisplay.js
@@ -34,7 +34,7 @@ function ImageDisplay({ projectName }) {
 
     try {
       // Send file to backend using fetch
-      const response = await fetch('https://brain-tumor-segmentation-cfzs.onrender.com/upload_dicom', {
+      const response = await fetch('https://gvraviram2003.pythonanywhere.com/upload_dicom', {
         method: 'POST',
         body: formData,
       });
